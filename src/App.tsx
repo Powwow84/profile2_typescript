30c73f7956f7514dfe86751ffda01dc843449971
Powwow84 <pao.saelee84@gmail.com>
2023-09-14 11:46:05 -0700

    first commit"

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import './App.css'
+import NavBar from './components/navBar'
+
+function App() {
+  return(
+    <>
+    <Router>
+      <NavBar>
+
+      </NavBar>
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
